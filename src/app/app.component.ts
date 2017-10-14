16bff4a1736fa70cfcf9b431f196f0dc21b34cd3
Chirag Sane <chiragsane333@gmail.com>
2017-10-14 09:44:07 +0530

    added dark theme option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  darkTheme = false;
+  toggleDarkTheme = () => {
+    this.darkTheme = !this.darkTheme;
+  }
 }
